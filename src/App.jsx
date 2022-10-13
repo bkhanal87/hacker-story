acf9eb97dc107b0d7673cff09f815ed321f5aa84
bkhanal87@gmail.com <bkhanal87@gmail.com>
2022-10-12 22:23:14 -0400

    render a list of items

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import * as React from 'react';
 import './App.css';
 
+// create list here
+const list = [
+  {
+    title: 'react',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 
 const welcome = {
-  greeting: "Hey",
+greeting: "Hey",
 };
 
 function getTitle(title) {
-  return title;
+return title;
 }
 
 /* remove boilerplate code and reduce the component to a lightweight version.
@@ -26,6 +46,23 @@ function App() {
       Since JSX is closer to JS than to HTML, React uses the camelCase naming convention */}
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return (
+          <li key={item.objectID}>
+            <span>
+              <a href= {item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        )})}
+      </ul>
+
     </div>
   );
 }
