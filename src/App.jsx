c7843d45be857e463279bc6faa05b5354885362f
bkhanal87@gmail.com <bkhanal87@gmail.com>
2022-10-13 22:08:10 -0400

    props

--- src/App.jsx
+++ src/App.jsx
@@ -33,23 +33,41 @@ return title;
 
 /* remove boilerplate code and reduce the component to a lightweight version.
 App is basically a JavaScript function aka a functional component. defined in PascalCase. */
-const App = () =>
-/* here, can have implementation details */
+const App = () => {
+  /* here, can have implementation details */
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  return (
+    /* whatever is returned here resembles HTML but it is JSX code, the syntax that allows to combine JS and HTML. 
+    While HTML can be used almost (except for the attribute) in its native way in JSX, everything in curly braces can be used to interpolate JS. */
 
-(
-  /* whatever is returned here resembles HTML but it is JSX code, the syntax that allows to combine JS and HTML. 
-  While HTML can be used almost (except for the attribute) in its native way in JSX, everything in curly braces can be used to interpolate JS. */
+    <div>
+      <h1>{welcome.greeting} {getTitle('React')}</h1>
 
-  <div>
-    <h1>{welcome.greeting} {getTitle('React')}</h1>
+      <Search />
 
-    <Search />
+      <hr />
 
-    <hr />
-
-    <List />
-  </div>
-);
+      <List list={stories} />
+    </div>
+  );
+};
 
 {/* Search component for label and input */}  
 const Search = () => {
@@ -75,19 +93,22 @@ export default App;
 
 {/* new List components created to encapsulate functionalities */}
 
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item}/>
     ))}
   </ul>
 );
 
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
 
